60b537c07c11c7d492687f0a38f4589ee566e507
favolia <daisszo1455@gmail.com>
2023-06-27 21:24:58 +0800

    -

--- src/components/MainCover.vue
+++ src/components/MainCover.vue
@@ -1,13 +1,13 @@
 <template>
-  <section class="w-full bg-couples bg-cover h-screen">
+  <section class="w-full bg-couples bg-center bg-cover h-screen">
     <section class="container-section h-screen grid content-end">
       <div 
         class="animate__animated animate__fadeInUpBig text-gray-100 text-center bg-gray-800 bg-opacity-50 backdrop-blur-sm rounded-t-3xl px-6 py-10">
         <p>Pernikahan</p>
-        <h1 class="tangerine-font text-title sm:text-4xl font-bold">{{ couples[0] }}</h1>
+        <h1 class="tangerine-font text-4xl font-bold">{{ couples[0] }}</h1>
         <sup class="font-serif font-normal text-lg">(Zul)</sup>
         <h1 class="tangerine-font text-title leading-[3rem]">&</h1>
-        <h1 class="tangerine-font text-title sm:text-4xl font-bold">{{ couples[1] }}</h1>
+        <h1 class="tangerine-font text-4xl font-bold">{{ couples[1] }}</h1>
         <sup class="font-serif font-normal text-lg">(Amel)</sup>
         <decoration class="fill-amber-400 mx-auto w-3/12 mb-3"></decoration>
         <p class="text-sm">
