eed7d9f9b61e7dc49e1b66d74358836f00e9bece
favolia <daisszo1455@gmail.com>
2023-06-29 21:04:45 +0800

    add smooth transition on  undangan.vue

--- src/components/Undangan.vue
+++ src/components/Undangan.vue
@@ -1,41 +1,44 @@
 <template>
-    <section v-if="showUndangan" class="fixed w-full left-0 top-0 h-screen z-50 bg-black flex flex-col justify-center items-center">
-      <h1 class="tangerine-font text-2xl text-white" data-aos="fade-up">Undangan Pernikahan</h1>
-      
-      <div class="w-44 h-44 mt-5 mb-2 rounded-full overflow-hidden flex justify-center items-center outline outline-white" data-aos="fade-up">
-        <img src="/galleries/a6.jpg" alt="">
-      </div>
-  
-      <decoration class="fill-amber-400 mx-auto w-3/12 lg:w-1/12" data-aos="fade-up"></decoration>
-      <p class="text-sm flex flex-col justify-center text-gray-100 text-center" data-aos="fade-up">
-        <p class="font-medium">Kepada Bapak/Ibu Yang Terhormat</p>
-        <p class="my-2 text-lg font-bold">{{ query.to }}</p>
-        <small>Izinkan kami turut mengundang anda dalam acara kami</small>
-      </p>
-  
-      <button class="mt-3 border border-white px-2 py-1 rounded-md text-white hover:bg-white duration-300 hover:text-black" @click="hideUndangan">Buka Undangan</button>
-    </section>
-  </template>
-  
-  <script setup>
-  import { computed, ref, onMounted } from 'vue'
-  import { useRoute } from 'vue-router'
-  import decoration from '@/assets/svg/decoration-2.svg'
-  
-  const route = useRoute()
-  let query = computed(() => route.query)
-  
-  if (route.query.to === undefined) query = { to: 'Saudara/i' }
-  
-  const showUndangan = ref(true)
-  
-  const hideUndangan = () => {
+  <section v-if="showUndangan" :class="hide ? 'fixed w-full left-0 top-0 h-screen z-50 bg-black flex flex-col justify-center items-center transition-all ease-in-out duration-1000' : 'fixed w-full left-0 top-[100%] h-screen z-50 bg-black flex flex-col justify-center items-center transition-all duration-300'">
+    <h1 class="tangerine-font text-2xl text-white" data-aos="fade-up">Undangan Pernikahan</h1>
+    
+    <div class="w-44 h-44 mt-5 mb-2 rounded-full overflow-hidden flex justify-center items-center outline outline-white" data-aos="fade-up">
+      <img src="/galleries/a6.jpg" alt="">
+    </div>
+
+    <decoration class="fill-amber-400 mx-auto w-3/12 lg:w-1/12" data-aos="fade-up"></decoration>
+    <p class="text-sm flex flex-col justify-center text-gray-100 text-center" data-aos="fade-up">
+      <p class="font-medium">Kepada Bapak/Ibu Yang Terhormat</p>
+      <p class="my-2 text-lg font-bold">{{ query.to }}</p>
+      <small>Izinkan kami turut mengundang anda dalam acara kami</small>
+    </p>
+
+    <button class="mt-3 border border-white px-2 py-1 rounded-md text-white hover:bg-white duration-300 hover:text-black" @click="hideUndangan">Buka Undangan</button>
+  </section>
+</template>
+
+<script setup>
+import { computed, ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import decoration from '@/assets/svg/decoration-2.svg'
+
+const route = useRoute()
+let query = computed(() => route.query)
+
+if (route.query.to === undefined) query = { to: 'Saudara/i' }
+
+const showUndangan = ref(true)
+const hide = ref(true)
+
+const hideUndangan = () => {
+  document.body.style.overflow = 'auto'
+  hide.value = false
+  setTimeout(() => {
     showUndangan.value = false
-    document.body.style.overflow = 'auto'
-  }
-  
-  onMounted(() => {
-    document.body.style.overflow = 'hidden'
-  })
-  </script>
-  
+  }, 1200)
+}
+
+onMounted(() => {
+  document.body.style.overflow = 'hidden'
+})
+</script>
